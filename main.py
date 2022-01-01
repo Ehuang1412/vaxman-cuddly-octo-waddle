2c8e4f64526c003388e45e2da2a2e36570a9ed21
Emily <eh1691@Nyu.edu>
2022-01-01 10:02:57 +0000

    process events

--- main.py
+++ main.py
@@ -2,18 +2,46 @@
 
 # Import and initialize the pygame library
 import pygame
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
 pygame.init()
 
+# Define constants for the screen width and height
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 250
+
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 # Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([500, 400])
 
 # Run until the user asks to quit
 running = True
+# Main loop
 while running:
-
-    # Did the user click the window close button?
+    # Look at every event in the queue
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
             running = False
 
     # Fill the background with white
